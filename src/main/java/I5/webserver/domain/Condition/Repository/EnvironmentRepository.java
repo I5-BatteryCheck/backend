816e839ac8716b862f7a024b7f2dd8b4d6f9abff
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-07-31 13:53:43 +0900

    refactor: 환경 도메인 제거

--- src/main/java/I5/webserver/domain/Condition/Repository/EnvironmentRepository.java
+++
@@ -1,9 +0,0 @@
-package I5.webserver.domain.Condition.Repository;
-
-import I5.webserver.domain.Condition.Entity.Environment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnvironmentRepository extends JpaRepository<Environment, Long> {
-}
